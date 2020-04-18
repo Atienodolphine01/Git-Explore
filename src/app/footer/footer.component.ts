842cf7b65ff906157e168935a9976d98177df0dc
Dolphine Atieno <awuordolphine65@gmail.com>
2020-04-18 18:11:30 +0300

    editted readme.md file

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  footcontent= '2019 Copyright:AtyenoD';
   constructor() { }
 
   ngOnInit() {
--- src/app/highlighter.directive.ts
+++ src/app/highlighter.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlighter]'
@@ -6,5 +6,18 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlighterDirective {
 
   constructor(private elem:ElementRef) { }
-
+  @HostListener("click") onClicks(){
+    this.textDeco("aqua")
+    }
+  
+    @HostListener("dblclick") onDoubleClicks(){
+      this.textDeco("transparent")
+    }
+  
+  
+    private textDeco(action:string){
+      this.elem.nativeElement.style.backgroundColor=action;
+  
+    }
+  
 }
