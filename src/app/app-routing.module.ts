1304f1ad446c338ba59527ca54458f38cc614198
Dolphine Atieno <awuordolphine65@gmail.com>
2020-04-18 07:28:55 +0300

    insert environment.ts file to gitignore file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersprofileComponent } from './usersprofile/usersprofile.component';
+import { RepolistComponent } from './repolist/repolist.component';
+import { FofourpageComponent } from './fofourpage/fofourpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'usersprofile', component: UsersprofileComponent},
+  {path: 'repolist', component: RepolistComponent},
+  {path: '**', component: FofourpageComponent},
+  {path: '', redirectTo: '/usersprofile', pathMatch: 'Full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
